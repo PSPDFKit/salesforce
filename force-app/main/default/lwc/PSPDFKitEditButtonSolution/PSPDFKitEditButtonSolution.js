4d81042b1b9e8481691951d9384d91f1f3d64ed3
Miguel Calderón <miguel@pspdfkit.com>
2022-10-04 22:26:59 +0200

    Rename files and add corresponding PSPDFKit local version.

---
+++ force-app/main/default/lwc/PSPDFKitEditButtonSolution/PSPDFKitEditButtonSolution.js
@@ -0,0 +1,25 @@
+import { LightningElement, api, track } from 'lwc';
+import Button_label from '@salesforce/label/c.Button_label';
+export default class PSPDFKitEditButtonSolution extends LightningElement {
+
+    @api recordId;
+    @api btnColor;
+    @api newTab = false;
+    @track newWindow
+    @track btnLabel = Button_label;
+
+    renderedCallback(){
+        this.template.querySelector("button").style="background-color:"+this.btnColor;
+    }
+
+    openFileInNewTab(){
+        if(this.newTab == true){
+            this.newWindow = window.open('/apex/PSPDFKit_InitPSPDFKit?id='+this.recordId,'_blank');
+        }else{
+            this.newWindow = window.open('/apex/PSPDFKit_InitPSPDFKit?id='+this.recordId,'_self');
+        }
+
+    }
+
+
+}
