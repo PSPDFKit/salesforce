605540b1a6e9af9c00450050d6ce65b3dc4bbe44
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2022-10-06 09:11:44 -0700

    update naming

--- force-app/main/default/lwc/PSPDFKitOpenSalesforceFiles/PSPDFKitOpenSalesforceFiles.js
+++ force-app/main/default/lwc/PSPDFKitOpenSalesforceFiles/PSPDFKitOpenSalesforceFiles.js
@@ -2,7 +2,7 @@ import { LightningElement, wire, track } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import getAttacmentDetails from '@salesforce/apex/PSPDFKitController.getAttacmentDetails';
 
-export default class PSPDFKitOpenSalesforceFiles extends LightningElement {
+export default class pspdfkitOpenSalesforceFiles extends LightningElement {
 
     @track isModalOpen;
     @track recordId;
