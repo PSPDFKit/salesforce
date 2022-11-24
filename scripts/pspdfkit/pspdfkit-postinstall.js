f7830c5a0a521a470867f1fb2f887e6b119e0237
Miguel Calderón <miguel@pspdfkit.com>
2022-11-24 14:09:33 +0100

    Revert "Add support for baseCoreUrl."
    This reverts commit 66408a2508c65e2095696fc01b64678516d12e2a.

--- scripts/pspdfkit/pspdfkit-postinstall.js
+++ scripts/pspdfkit/pspdfkit-postinstall.js
@@ -3,22 +3,18 @@ const fs = require("fs");
 const path = require("path");
 const AdmZip = require("adm-zip");
 
-const originSrc = "./node_modules/pspdfkit/dist/modern"
 const staticResourcesDir = "./force-app/main/default/staticresources"
 const pspdfkitJsDest = `${staticResourcesDir}/PSPDFKit.js`
 const pspdfkitLibDest = `${staticResourcesDir}/PSPDFKit_lib/modern/pspdfkit-lib`
-const pspdfkitCoreDest= `${staticResourcesDir}/PSPDFKit_core/modern/pspdfkit-lib`
 
 fs.rmSync(pspdfkitLibDest, { recursive: true, force: true });
-fs.rmSync(pspdfkitCoreDest, { recursive: true, force: true });
 fs.rmSync(pspdfkitJsDest, { force: true });
 
 fs.mkdirSync(pspdfkitLibDest, { recursive: true });
-fs.mkdirSync(pspdfkitCoreDest, { recursive: true });
 
 // Copy the pspdfkit-lib files used by the Salesforce integration to the static resources folder
 ncp(
-  `${originSrc}/pspdfkit-lib`,
+  "./node_modules/pspdfkit/dist/modern/pspdfkit-lib/",
   pspdfkitLibDest,
   {
 	filter(filepath) {
@@ -42,9 +38,13 @@ ncp(
 			return true
 		} else if (path.extname(filepath) === '.css' && !filename.startsWith('windows-')) {
 			return true
-		} else if (path.extname(filepath) === '.woff') {
+		} else if (path.extname(filepath) === '.wasm') {
 			return true
 		} else if (path.extname(filepath) === '.woff2') {
+			return true
+		} else if (filename.endsWith('.wasm.js')) {
+			return true
+		} else if (filename.endsWith('.wasm')) {
 			return true
 		} else if (filename === 'pspdfkit-lib') {
 			return true
@@ -68,41 +68,7 @@ ncp(
   }
 );
 
-// Copy the pspdfkit-lib Core assets used by the Salesforce integration to the static resources folder
-ncp(
-	`${originSrc}/pspdfkit-lib`,
-	pspdfkitCoreDest,
-	{
-	  filter(filepath) {
-		  const filename = path.basename(filepath);
-
-		  if (path.extname(filepath) === '.wasm') {
-			  return true
-		  } else if (filename.endsWith('.wasm.js')) {
-			  return true
-  		  } else if (filename === 'pspdfkit-lib') {
-     		return true
-		  }
-
-		  return false
-	  }
-	},
-	(err) => {
-	  err && console.error(err);
-
-	  const PSPDFKit_coreZipDest = `${staticResourcesDir}/PSPDFKit_core.zip`
-
-	  fs.rmSync(PSPDFKit_coreZipDest, { force: true });
-
-	  const zip = new AdmZip();
-
-	  zip.addLocalFolder(`${staticResourcesDir}/PSPDFKit_core`)
-
-	  zip.writeZip(PSPDFKit_coreZipDest);
-	}
-  );
-
 // Copy the main pspdfkit.js bundle to the static resources folder
-ncp(`${originSrc}/pspdfkit.js`, pspdfkitJsDest, (err) => {
+ncp("./node_modules/pspdfkit/dist/modern/pspdfkit.js", pspdfkitJsDest, (err) => {
   err && console.error(err);
 });
